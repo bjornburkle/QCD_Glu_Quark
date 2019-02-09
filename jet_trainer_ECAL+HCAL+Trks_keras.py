2b55bf544c524bf36c12c0371a3431449d06befe
Bjorn Burkle <bbbjornburkle@gmail.com>
2019-02-08 18:32:17 -0600

    Changed script so it reads imputs using tf api. Still need to make edits to callbacks to accomodate these changes

--- jet_trainer_ECAL+HCAL+Trks_keras.py
+++ jet_trainer_ECAL+HCAL+Trks_keras.py
@@ -3,9 +3,9 @@ np.random.seed(0)
 import os, glob
 import time
 import h5py
-import keras
+import tensorflow.keras as keras
 import math
-from keras.utils.io_utils import HDF5Matrix
+#from tensorflow.keras.utils.io_utils import HDF5Matrix
 import tensorflow as tf
 from sklearn.metrics import roc_curve, auc
 
@@ -23,24 +23,19 @@ if __name__ == '__main__':
     resblocks = args.resblocks
     epochs = args.epochs
 
-expt_name = 'keras_test'
+expt_name = 'quark-gluon'
 #expt_name = 'ResNet_blocks%d_RH1o100_ECAL+HCAL+Trk_lr%s_gamma0.5every10ep_epochs%d'%(resblocks, str(lr_init), epochs)
 
-#datafile = 'IMG/test_BoostedJets.hdf5'
-#datafile = 'root://cmsxrootd.fnal.gov//store/user/bburkle/E2E/BoostedJets_AllJets.hdf5'
-#datafile = '/eos/uscms/store/user/bburkle/E2E/BoostedJets_AllJets.hdf5'
-datafile = '/uscms/home/bburkle/nobackup/working_area/ml_code/QCD_Glu_Quark/data/BoostedJets_AllJets_Compressed_file-1.hdf5'
+#datafile = '/uscms/home/bburkle/nobackup/working_area/ml_code/QCD_Glu_Quark/data/BoostedJets_AllJets_Compressed_file-1.hdf5'
+datafile = '/uscms/home/bburkle/nobackup/working_area/ml_code/QCD_Glu_Quark/data/quark-gluon_train-set_n793900.hdf5'
 #data_dir = '/eos/uscms/store/user/bburkle/E2E'
 #datafile = glob.glob('%s/*.hdf5'%data_dir)
 
-def hdf5Dataset(filename, start, end):
-    x = HDF5Matrix(filename, 'X_jets', start=start, end=end)
-    # These steps are being moved to hdf5 conversion step
-    #x[x < 1.e-3] = 0. # Zero-Suppresion
-    #x[-1,...] = 25.*x[-1,...] # For HCAL: to match pixel intensity distn of other layers
-    #x = x/100. # To standardize
-    y = HDF5Matrix(filename, 'y', start=start, end=end)
-    return x, y
+
+#def hdf5Dataset(filename, start, end):
+#    x = HDF5Matrix(filename, 'X_jets', start=start, end=end)
+#    y = HDF5Matrix(filename, 'y', start=start, end=end)
+#    return x, y
 
 def val_set(filename, start, end):
     f = h5py.File(filename, 'r')
@@ -51,76 +46,50 @@ def val_set(filename, start, end):
     f.close()
     return (x, y, pt, m0)
 
-def train_set(filename, start, end):
+def train_set(filename, start, end, chunk):
     f = h5py.File(filename, 'r')
-    x = f['X_jets'][start:end]
-    y = f['y'][start:end]
+    #x = np.asarray(f['X_jets'][start:end], dtype=np.float32)
+    #y = np.asarray(f['y'][start:end], dtype=np.uint8)
+    x = np.empty(shape=(end-start,125,125,3), dtype=np.float32)
+    y = np.empty(shape=(end-start), dtype=np.uint8)
+    chunks  = list(range(start, end, int((end-start)/chunk)))
+    #print(chunks)
+    for chunk_start, chunk_end in zip(chunks[:-1], chunks[1:]):
+        print('loading chunks %d-%d'%(chunk_start, chunk_end))
+        x[chunk_start-start:chunk_end-start] = f['X_jets'][chunk_start:chunk_end]
+        y[chunk_start-start:chunk_end-start] = f['y'][chunk_start:chunk_end]
     f.close()
     return x, y
 
-# For this to work right, may need to split training, validation, and testing data sets into 3 separate input files
-# Can also put the different datasets as different groups in the same hdf5 file
-class DataGenerator(keras.utils.Sequence):
-    def __init__(self, filename, batch_size=32, data_split=100, start=0, end=32*100, shuffle=True):
-        self.hf = h5py.File(filename, 'r')
-        self.x_jets = np.float32(self.hf['X_jets'][start:end])
-        self.y = np.float32(self.hf['y'][start:end])
-        #self.total_len = len(self.hf['y'])
-        self.total_len = end - start
-        self.start = start
-        self.end = end
-        self.batch_size = batch_size
-        self.shuffle = shuffle
-        self.idx = 0
-        self.data_split = data_split
-        self.len_segment = math.floor(self.total_len / data_split)
-        self.seg_list = list(range(0, self.total_len, self.len_segment))
-        print(self.total_len, self.len_segment)
-        print(self.seg_list)
-        self.cur_seg_idx = 0
-        #self.x_cur = self.hf['X_jets'][self.start:self.end][:self.len_segment]
-        self.x_cur = self.x_jets[:self.len_segment]
-        #self.y_cur = self.hf['y'][self.start:self.end][:self.len_segment]
-        self.y_cur = self.y[:self.len_segment]
+def image_data_generator(x, y, is_training=True, batch_sz=32):
 
-    def __len__(self):
-        return self.data_split
+    w = 125
+    h = 125
+    channels = 3
+    classes=2
 
-    def next_seg(self):
-        #self.cur_seg_idx += self.len_segment
-        #print(self.cur_seg_idx)
-        chunk_start = self.seg_list[self.cur_seg_idx]
-        #chunk_end = self.seg_list[self.cur_seg_idx+1]
-        self.cur_seg_idx += 1
-        chunk_end = chunk_start + self.len_segment
-        self.x_cur = self.x_jets[chunk_start:chunk_end]
-        self.y_cur = self.y[chunk_start:chunk_end]
-        if self.cur_seg_idx == len(self.seg_list)-1:
-            self.cur_seg_idx = 1
-            self._on_epoch_end()
+    def map_fn(image, label):
+        #x = tf.reshape(tf.cast(image, tf.float32), (w,h,channels))
+        x = image
+        y = tf.one_hot(tf.cast(label, tf.uint8), classes)
+        return x, y
 
-    def _on_epoch_end(self):
-        if self.shuffle:
-           #print('Shuffle Data Segments')
-           np.random.shuffle(self.seg_list) 
+    dataset = tf.data.Dataset.from_tensor_slices((x, y))
+    if is_training:
+        dataset = dataset.shuffle(10000)
+        #pass
+    dataset = dataset.apply(tf.contrib.data.map_and_batch(
+        map_fn, batch_sz,
+        #num_parallel_batches=4,
+        drop_remainder=True if is_training else False))
+    #dataset = dataset.map(map_fn)
+    #dataset = tf.map_fn(map_fn, dataset)
+    #dataset = dataset.batch(batch_sz)
+    dataset = dataset.repeat()
+    dataset = dataset.prefetch(tf.contrib.data.AUTOTUNE)
 
-    #def __getitem__(self, idx):
-    def generate(self):
-        while 1:
-            idx = self.idx
-            if idx >= self.len_segment:
-                self.next_seg()
-                idx = 0
+    return dataset
 
-            if idx + self.batch_size >= self.len_segment:
-                batch_x = self.x_cur[idx:]
-                batch_y = self.y_cur[idx:]
-            else:
-                batch_x = self.x_cur[idx:(idx + self.batch_size)]
-                batch_y = self.y_cur[idx:(idx + self.batch_size)]
-            self.idx = idx + self.batch_size
-            #return batch_x, batch_y
-            yield batch_x, batch_y
 
 # After N batches, will output the loss and accuracy of the last batch tested
 class NBatchLogger(keras.callbacks.Callback):
@@ -151,7 +120,7 @@ class SaveEpoch(keras.callbacks.Callback):
         fpr, tpr, _ = roc_curve(y, y_pred)
         roc_auc = auc(fpr, tpr)
         #roc_auc = 0.
-        #print('\nVal Accuracy --- %.4f\tVal Loss ---- %.4f' % (acc, loss))
+        print('\nVal Accuracy --- %.4f\tVal Loss ---- %.4f' % (acc, loss))
         print('AUC for epoch %d is: %.4f, best is: %.4f\n' % (epoch+1, roc_auc, self.auc_best))
         if roc_auc > self.auc_best:
             self.auc_best = roc_auc
@@ -176,9 +145,11 @@ def LR_Decay(epoch):
 # Test input file is size 32000
 #train_sz = 10400*2
 #train_sz = 704000
-train_sz = 320000
-valid_sz = 48000
-test_sz = 48000
+#train_sz = 320000
+train_sz = 3200#384000*2 #qg amount in paper
+valid_sz = 32*100#12950 # qg amount in paper
+test_sz = 69653 #qg amount in paper
+BATCH_SZ = 32
 
 
 if __name__ == '__main__':
@@ -190,13 +161,20 @@ if __name__ == '__main__':
             os.makedirs('%s/%s'%(d, expt_name))
 
     #train_x, train_y = hdf5Dataset(datafile, 0, train_sz)
-    train_x, train_y = train_set(datafile, 0, train_sz)
-    val_data = val_set(datafile, start=train_sz, end=train_sz+valid_sz)
-    #training_generator = DataGenerator(datafile, batch_size=32, data_split=train_sz/32/10, start=0, end=train_sz).generate()
-    #validation_generator = DataGenerator(datafile, batch_size=32, data_split=10, start=train_sz, end=train_sz+valid_sz).generate()
-    #val_x, val_y = hdf5Dataset(datafile, train_sz, train_sz+valid_sz)
+    train_x, train_y = train_set(datafile, 0, train_sz, 4)
+    train_x_placeholder = tf.placeholder(train_x.dtype, train_x.shape)
+    train_y_placeholder = tf.placeholder(train_y.dtype, train_y.shape)
+    train_data = image_data_generator(train_x_placeholder, train_y_placeholder, is_training=True, batch_sz=BATCH_SZ)
+    #train_data = image_data_generator(train_x, train_y, is_training=True, batch_sz=BATCH_SZ)
+    val_x, val_y = train_set(datafile, train_sz, train_sz+valid_sz, 4)
+    val_x_placeholder = tf.placeholder(val_x.dtype, val_x.shape)
+    val_y_placeholder = tf.placeholder(val_y.dtype, val_y.shape)
+    val_data = image_data_generator(val_x, val_y, is_training=False)
+    #val_data = image_data_generator(val_x_placeholder, val_y_placeholder, is_training=False)
+    #val_data = val_set(datafile, start=train_sz, end=train_sz+valid_sz)
 
-    from keras.backend.tensorflow_backend import set_session
+    #from tensorflow.keras.backend.tensorflow_backend import set_session
+    from tensorflow.keras.backend import set_session
     config = tf.ConfigProto()
     config.gpu_options.per_process_gpu_memory_fraction = 0.3
     set_session(tf.Session(config=config))
@@ -218,17 +196,35 @@ if __name__ == '__main__':
     # Model Callbacks
     print_step = 1000
     #checkpoint = SaveEpoch((val_x, val_y), expt_name)
-    checkpoint = SaveEpoch(val_data, expt_name)
+    #checkpoint = SaveEpoch(val_data, expt_name)
     batch_logger = NBatchLogger(display=print_step)
     csv_logger = keras.callbacks.CSVLogger('%s.log'%(expt_name), separator=',', append=False)
     lr_scheduler = keras.callbacks.LearningRateScheduler(LR_Decay)
     #callbacks_list=[checkpoint, batch_logger, csv_logger, lr_scheduler]
-    callbacks_list=[checkpoint, csv_logger, lr_scheduler]
+    #callbacks_list=[checkpoint, csv_logger, lr_scheduler]
     #callbacks_list=[checkpoint, csv_logger]
 
-    #history = resnet.fit_generator(training_generator, steps_per_epoch=train_sz/32, epochs=epochs, verbose=1, callbacks=callbacks_list, workers=10, initial_epoch=args.load_epoch, shuffle=False)
-    #history = resnet.fit_generator(training_generator, steps_per_epoch=train_sz/32, epochs=epochs, verbose=1, validation_data=validation_generator, validation_steps=10, callbacks=callbacks_list, workers=10, initial_epoch=args.load_epoch, shuffle=False)
-    history = resnet.fit(x=train_x, y=train_y, batch_size=32, epochs=epochs, verbose=1, callbacks=callbacks_list, validation_data=(val_data[0], val_data[1]), shuffle='batch', initial_epoch = args.load_epoch)
+    #history = resnet.fit(x=train_x, y=train_y, batch_size=32, epochs=epochs, verbose=1, callbacks=callbacks_list, validation_data=(val_data[0], val_data[1]), shuffle='batch', initial_epoch = args.load_epoch)
+    #history = resnet.fit(train_data.make_initializer_iterator(), steps_per_epoch=train_sz/32, epochs=epochs, verbose=1, callbacks=callbacks_list, initial_epoch = args.load_epoch, shuffle='batch')
+    train_iterator = train_data.make_initializable_iterator()
+    #val_iterator = val_data.make_initializable_iterator()
+    with tf.Session() as sess:
+        sess.run(train_iterator.initializer, feed_dict={
+            train_x_placeholder: train_x,
+            train_y_placeholder: train_y})
+            #val_x_placeholder: val_x,
+            #val_y_placeholder: val_y})
+        history = resnet.fit(
+            #train_data.make_one_shot_iterator(),
+            train_iterator,
+            steps_per_epoch = train_sz / 32,
+            epochs=epochs,
+            #validation_data = val_data,#val_iterator,
+            #validation_steps = valid_sz / 32,
+            #callbacks=callbacks_list,
+            verbose=1,
+            initial_epoch = args.load_epoch)
+
 
     print('Network has finished training')
 
