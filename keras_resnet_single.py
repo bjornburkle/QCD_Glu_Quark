be380b7a316142291cba3d5ccab1a61dc05cf1d8
Bjorn Burkle <bbbjornburkle@gmail.com>
2019-02-08 18:30:44 -0600

    Changed final dense layer to have depth 2 to accomodate one-hot labels

--- keras_resnet_single.py
+++ keras_resnet_single.py
@@ -1,11 +1,14 @@
-import keras
-from keras.models import Sequential
-from keras import layers
-from keras.layers.merge import add
+#import keras
+#from keras.models import Sequential
+#from keras import layers
+#from keras.layers.merge import add
+
+import tensorflow.keras as keras
+from tensorflow.keras.models import Sequential
+from tensorflow.keras import layers
 
 # TODO
 # For kernel size and stride, went from a single value 'n' to a tuple '(n,n)'. Need to make sure that this is the correct way to go from pytorch to keras
-#def ResBlock(x, in_channels, out_channels):
 def ResBlock(in_channels, out_channels):
 
     def f(x):
@@ -13,27 +16,23 @@ def ResBlock(in_channels, out_channels):
 
         downsample = out_channels//in_channels
 
-        #TODO check the way that these layers are working, make sure it is done properly
         #TODO see if I want the batch normalization layers
-        conv = layers.Conv2D(out_channels, input_shape=keras.backend.shape(x)[1:], activation='relu', kernel_size=(3,3), strides=(downsample,downsample), padding='SAME')(x)
+        conv = layers.Conv2D(out_channels, activation='relu', kernel_size=(3,3), strides=(downsample,downsample), padding='SAME')(x)
+        #conv = layers.Conv2D(out_channels, input_shape=keras.backend.shape(x)[1:], activation='relu', kernel_size=(3,3), strides=(downsample,downsample), padding='SAME')(x)
         #conv = layers.BatchNormalization()(conv)
         conv = layers.Conv2D(out_channels, kernel_size=(3,3), padding='SAME')(conv)
         #conv = layers.BatchNormalization()(conv)
-        #shortcut = layers.Conv2D(out_channels, kernel_size=1, strides=downsample)(x)
 
         if downsample > 1:
-            #residual = shortcut
             residual = layers.Conv2D(out_channels, kernel_size=1, strides=downsample)(x)
 
         block = layers.Add()([conv, residual])
-        #block = conv + residual
         block = layers.Activation('relu')(block)
 
         return block
     return f
 
 #ResBlocks
-#def block_layers(_x, _nblocks, _fmaps):
 def block_layers(nblocks, fmaps):
     def f(x):
         for _ in range(nblocks):
@@ -42,7 +41,6 @@ def block_layers(nblocks, fmaps):
     return f
 
 
-#def ResNet(in_channels, nblocks, fmaps):
 class ResNet(object):
 
     @staticmethod
@@ -52,7 +50,7 @@ class ResNet(object):
         #conv0 - changed padding from 1 to 'SAME'
         x = layers.Conv2D(fmaps[0], input_shape=input_shape, kernel_size=(7,7), strides=(2,2), padding='SAME')(input)
         x = layers.Activation('relu')(x)
-        x = layers.MaxPooling2D(pool_size=2)(x) #TODO make sure that pool_size is the same as kernal_size
+        x = layers.MaxPooling2D(pool_size=2)(x)
 
         x = block_layers(nblocks, [fmaps[0],fmaps[0]])(x)
         x = block_layers(1, [fmaps[0],fmaps[1]])(x)
@@ -61,7 +59,7 @@ class ResNet(object):
         #TODO get pool size
         x = layers.MaxPooling2D()(x)
         x = layers.Flatten()(x)
-        predictions = layers.Dense(1)(x)
+        predictions = layers.Dense(2)(x)
         predictions = layers.Activation('sigmoid')(predictions)
 
         model = keras.Model(inputs=input, outputs=predictions)
