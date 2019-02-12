ca8138ce333babdd22fbd696daaa3610126eaa0b
Bjorn Burkle <bbbjornburkle@gmail.com>
2019-02-12 10:04:19 -0600

    now works for changes made to jet trainer

--- jet_eval_keras.py
+++ jet_eval_keras.py
@@ -2,11 +2,12 @@ import h5py
 import os, glob, re
 import tensorflow as tf
 from sklearn.metrics import roc_curve, auc
-import keras
+import tensorflow.keras as keras
 import keras_resnet_single as networks
 
 #from jet_trainer_ECAL+HCAL+Trks_keras import train_sz, valid_sz, test_sz, val_set, datafile
 from jet_trainer_imports import train_sz, valid_sz, test_sz, val_set, datafile
+testfile = '/uscms/home/bburkle/nobackup/working_area/ml_code/QCD_Glu_Quark/data/quark-gluon_test-set_n139306.hdf5'
 
 import argparse
 parser = argparse.ArgumentParser(description='Evaluation parameters.')
@@ -21,20 +22,23 @@ expt_name = args.expt_name
 nblocks = 3
 
 # Uncomment following lines if you dont want to use the vars from the training script
-#datafile = ''
 #train_sz = 0
 #valid_sz = 0
 #test_sz = 0
 
 #model_file = glob.glob('MODELS/%s/model_epoch%d_auc*.hdf5'%(expt_name, epoch))[0]
-model_file = glob.glob('MODELS/%s/epoch%d_auc*.hdf5'%(expt_name, epoch))[0]
+model_file = glob.glob('MODELS/%s/epoch%d_auc*.hdf5*'%(expt_name, epoch))
+assert len(model_file) == 2
+model_file = model_file[0].split('.hdf5')[0]+'.hdf5'
+assert model_file != ''
 for d in ['METRICS_TEST']:
     if not os.path.isdir('%s/%s'%(d, expt_name)):
         os.mkdir('%s/%s'%(d, expt_name))
 
-x, y, pt, m0 = val_set(datafile, start=train_sz+valid_sz, end=train_sz+valid_sz+test_sz)
+x, y, pt, m0 = val_set(testfile, start=0, end=test_sz)
 
-from keras.backend.tensorflow_backend import set_session
+#from keras.backend.tensorflow_backend import set_session
+from tensorflow.keras.backend import set_session
 config = tf.ConfigProto()
 config.gpu_options.per_process_gpu_memory_fraction = 0.3
 set_session(tf.Session(config=config))
@@ -44,9 +48,10 @@ resnet = networks.ResNet.build(3, nblocks, [16,32], (125,125,3))
 #resnet = keras.models.load_weights(model_name)
 resnet.load_weights(model_file)
 
-y_pred = resnet.predict(x, verbose=1).ravel()
+y_pred = resnet.predict(x, verbose=1)[:,1]
 fpr, tpr, _ = roc_curve(y, y_pred)
 roc_auc = auc(fpr, tpr)
+print('roc auc is:', roc_auc)
 
 score_str = 'epoch%d_auc%.4f'%(epoch, roc_auc)
 h = h5py.File('METRICS_TEST/%s/metrics_%s.hdf5'%(expt_name, score_str), 'w')
